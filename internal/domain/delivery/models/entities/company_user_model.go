4d0539ef7adddceef7b051f1609a243a22fb62a1
Marlon H. Garcia <marlon.hg2003@gmail.com>
2025-03-10 11:11:08 -0600

    refactor: correccion de relacion erronea generada por GORM y adicion de nuevos campos en el docker-compose.yml

--- internal/domain/delivery/models/entities/company_user_model.go
+++ internal/domain/delivery/models/entities/company_user_model.go
@@ -7,7 +7,7 @@ import (
 type CompanyUser struct {
 	UserID          string    `gorm:"column:user_id;type:char(36);primaryKey"`
 	CompanyID       string    `gorm:"column:company_id;type:char(36);primaryKey"`
-	BranchID        string    `gorm:"column:branch_id;type:char(36);"`
+	BranchID        string    `gorm:"column:branch_id;type:char(36);primaryKey"`
 	Position        string    `gorm:"column:position;type:varchar(100);not null"`
 	Department      string    `gorm:"column:department;type:varchar(100)"`
 	Permissions     string    `gorm:"column:permissions;type:json"`
@@ -16,9 +16,9 @@ type CompanyUser struct {
 	UpdatedAt       time.Time `gorm:"column:updated_at;type:timestamp;default:CURRENT_TIMESTAMP"`
 
 	// Relationships
-	CompanyBranch *Branch  `gorm:"foreignKey:CompanyID;references:ID"`
-	User          *User    `gorm:"foreignKey:UserID;references:ID"`
-	Company       *Company `gorm:"foreignKey:CompanyID;references:ID"`
+	Branch  *Branch  `gorm:"foreignKey:BranchID;references:ID"`
+	User    *User    `gorm:"foreignKey:UserID;references:ID"`
+	Company *Company `gorm:"foreignKey:CompanyID;references:ID"`
 }
 
 func (CompanyUser) TableName() string {
